1183dc05e1f5c22f983e1de1b2a55e3af25d3916
haaslaura <110221401+haaslaura@users.noreply.github.com>
2025-03-28 16:47:34 +0100

    Test config

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
-// On importe le package http de Node :
 const http = require('http');
-
 const app = require('./app');
-app.set('port', process.env.PORT || 3000);
 
-// La fonction ci-après est excécutée à chaque appel effectué sur ce serveur :
-// const server = http.createServer((req, res) => {
-//     res.end('Voilà la réponse du serveur')
-// });
+app.set('port', process.env.PORT || 3000);
 
 const server = http.createServer(app);
 
-// Le serveur doit écouter les requêtes envoyées :
 server.listen(process.env.PORT || 3000);
